7c0c920815bd04bb983d1e4e5e3a1f6ccad5a89a
nanditha <nanditha.daram3@gmail.com>
2025-11-08 19:55:39 +0530

    done

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -7,7 +7,6 @@ import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Profile from './pages/Profile.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-// 1. Import the new page component
 import BookDetailPage from './pages/BookDetailPage.jsx'; 
 
 const router = createBrowserRouter([
@@ -18,11 +17,7 @@ const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/login', element: <Login /> },
       { path: '/register', element: <Register /> },
-      
-      // 2. Add the new dynamic route. :bookId is a URL parameter
       { path: '/book/:bookId', element: <BookDetailPage /> }, 
-
-      // Protected Routes
       {
         element: <ProtectedRoute />,
         children: [
